99fe136aba15824f3a2ad1be38dcc2d7b7315913
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-03-11 14:56:29 +0800

    Batch Update

--- db.sql
+++ db.sql
@@ -1,9 +1,11 @@
+SET FOREIGN_KEY_CHECKS=0;
+
 CREATE TABLE IF NOT EXISTS `cms` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `key` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `value` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -12,27 +14,29 @@ CREATE TABLE IF NOT EXISTS `contact` (
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `message` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `document_type` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `downloadable` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `category` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `file_id` bigint unsigned NOT NULL,
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `file_id` (`file_id`)
+  KEY `file_id` (`file_id`),
+  CONSTRAINT `FK_downloadable_file` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `file` (
@@ -41,8 +45,8 @@ CREATE TABLE IF NOT EXISTS `file` (
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `size` bigint unsigned NOT NULL DEFAULT '0',
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -52,18 +56,20 @@ CREATE TABLE IF NOT EXISTS `log` (
   `loggable_type` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `loggable_id` bigint unsigned NOT NULL,
   `user_id` bigint unsigned NOT NULL,
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `loggable_id` (`loggable_id`,`user_id`) USING BTREE,
-  FULLTEXT KEY `loggable_type` (`loggable_type`)
+  KEY `FK_log_user` (`user_id`),
+  FULLTEXT KEY `loggable_type` (`loggable_type`),
+  CONSTRAINT `FK_log_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `queue` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `payload` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -77,40 +83,47 @@ CREATE TABLE IF NOT EXISTS `request` (
   `acknowledged` tinyint NOT NULL DEFAULT '0',
   `file_id` bigint unsigned DEFAULT NULL,
   `status` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Received',
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `created_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_request_user` (`user_id`),
+  KEY `FK_request_file` (`file_id`),
+  CONSTRAINT `FK_request_file` FOREIGN KEY (`file_id`) REFERENCES `file` (`id`),
+  CONSTRAINT `FK_request_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `storage` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `content` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `task` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `for` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `for` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `done` tinyint(1) NOT NULL DEFAULT '0',
   `request_id` bigint unsigned NOT NULL,
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `request_id` (`request_id`),
+  CONSTRAINT `FK_task_request` FOREIGN KEY (`request_id`) REFERENCES `request` (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `token` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `user_id` bigint unsigned NOT NULL,
   `hash` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `hash` (`hash`),
-  KEY `user_id` (`user_id`)
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `FK_token_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `user` (
@@ -121,9 +134,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `role` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `profile_picture_id` bigint unsigned DEFAULT NULL,
-  `created_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_at` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `student_id_number` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `updated_at` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `profile_picture_id` (`profile_picture_id`)
+  UNIQUE KEY `profile_picture_id` (`profile_picture_id`),
+  CONSTRAINT `FK_user_file` FOREIGN KEY (`profile_picture_id`) REFERENCES `file` (`id`)
 );
+
+SET FOREIGN_KEY_CHECKS=1;
