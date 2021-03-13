481c8b6bed0b7431472e961d54d32732257603d8
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-03-13 15:00:38 +0800

    Update

--- models/Request.php
+++ models/Request.php
@@ -118,7 +118,7 @@ class Request extends Model
                 queue()->register(new SendMail($request->user->email, 'emails.updated-request', 'Request Updated', $data));
 
                 $text  = 'Your Request (ID: ' . $request->request_id . ') has been updated to \'' . $request->status . '\'.';
-                $text .= sprintf('%s%s', config('app.frontend.url'), "/dashboard/requests/{$this->id}");
+                $text .= sprintf('%s%s', config('app.frontend.url'), "/dashboard/requests/{$request->id}");
 
                 queue()->register(new SendMessage($request->user->phone, $text));
             }
